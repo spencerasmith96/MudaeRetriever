ce6f5239e6897f516bfb9d182fd65f8b8b37d2fd
Spencer <spencerasmith96@gmail.com>
2021-06-03 01:08:07 -0400

    Refractored for reusability

--- MudaeRetriever.py
+++ MudaeRetriever.py
@@ -18,9 +18,94 @@ def logError(error: str):
     errorFile = open("errorlog.txt", "a", encoding='utf-8')
     currentTime = datetime.now().strftime("%H:%M:%S")
     currentDay = date.today().strftime("%m/%d/%Y")
-    fullErrorMessage = currentDay + currentTime + error + "\n"
+    fullErrorMessage = currentDay + " - " + currentTime + " - " + error + "\n"
     errorFile.write(fullErrorMessage)
     errorFile.close()
+
+async def retrieveMaxChars(channel, names):
+    """Calls and retrieves max number of characters from Mudae using Discord.
+    Stores in names on sucess or returns False."""
+    maxRetry = 3
+
+    ### Find Max number of characters ####
+    maxRankMessage = False
+    for retry in range(maxRetry):
+        names.requestCharacterNum()
+        
+        def check(message):
+            return message.author.id == mudaeID and on_message.names.validMax(message.content)
+
+        try:
+            maxRankMessage = await client.wait_for('message', check=check, timeout = 5.0)
+        except asyncio.TimeoutError:
+            await channel.send("Mudae timed out. Attempt (" + str(retry + 1) + "/" + str(maxRetry) + ")")
+
+        if(maxRankMessage != False): # Names retrieves successfully
+            break
+    
+    if(maxRankMessage == False): # Retries end, log error and quit
+        errorMsg = "Unable to retrieve max characters. Aborting."
+        await channel.send(errorMsg)
+        logError(errorMsg)
+        return False
+
+    ### Get max chars in form of an int ###
+    maxChars = names.parseMaxCharacters(maxRankMessage.content)
+    if(maxChars == False): # Unable to get the int
+        errorMsg = "Error, unable to parse max characters. Message: " + maxRankMessage.content
+        await channel.send(errorMsg)
+        logError(errorMsg)
+        return False
+
+    return maxChars
+
+async def retrieveNames(startRank: int, endRank: int, names, channel):
+    """Adds the names of characters from startRank to endRank into the names object, logs any error"""
+    maxRetry = 3
+
+    for rank in range(startRank, endRank):    
+        ### Retrieve character name for rank ###
+        charRankMessage = False
+        for retry in range(maxRetry):
+            sleep(1)
+            names.requestCharacter(rank)
+
+            def check(message):
+                return message.author.id == mudaeID and on_message.names.validCharacterPrefix(message.content, rank)
+
+            try:
+                charRankMessage = await client.wait_for('message', check=check, timeout = 2.0)
+            except asyncio.TimeoutError:
+                await channel.send("Mudae timed out. Retrying (" + str(retry + 1) + "/" + str(maxRetry) + ")")
+
+            if(charRankMessage != False): # Names retrieves successfully
+                break
+
+        if(charRankMessage == False):
+            errorMsg = "Mudae timed out for rank:" + str(rank)
+            await channel.send(errorMsg)
+            logError(errorMsg)
+            sleep(60)
+            continue
+
+        name = names.parseCharacterName(charRankMessage.content, rank)
+        if(name == False):
+            logError("Error requesting name from: " + charRankMessage.content)
+            continue
+
+        result = on_message.names.addName(name)
+        if(result == False):
+            logError("Unable to add name: " + name + " at rank: " + str(rank))
+            continue
+
+def saveNames(names):
+    """ Saves names to txt document """
+
+    print("Progress: ", (rank/names.maxRank) * 100, "%", sep='')
+
+    namesFile = open("names.txt", "w", encoding='utf-8')
+    namesFile.write("Characters: " + str(rank) + "\n" + str(names.names))
+    namesFile.close()
 
 @client.event
 async def on_message(message):
@@ -29,76 +114,14 @@ async def on_message(message):
     
     # Requests and compiles a list of all character names from Mudae
     if message.content.startswith(prefix + "getName"):
-
-        # Request max chars
-        maxRetry = 3
-        for retry in range(maxRetry):
-            on_message.names.requestCharacterNum()
-        
-            def check(message):
-                print("valid?")
-                print(message.content)
-                return message.author.id == mudaeID and on_message.names.validMax(message.content)
-
-            try:
-                maxRankMessage = await client.wait_for('message', check=check, timeout = 5.0)
-            except asyncio.TimeoutError:
-                await message.channel.send("Mudae timed out. Retrying (" + str(retry + 1) + "/" + str(maxRetry) + ")")
-                if(retry + 1 == maxRetry):
-                    return
-            else:
-                break
-
-
-        response = on_message.names.parseMaxCharacters(maxRankMessage.content)
-        if(response == False):
-            print("Error, unable to parse max characters. Message:", maxRankMessage.content)
+        maxChars = await retrieveMaxChars(message.channel, on_message.names)
+        if(maxChars == False):
             return
+        bulk = 100
+        for rankStep in range(1, maxChars, bulk):
+            await retrieveNames(1, maxChars, on_message.names, message.channel)
+            saveNames(on_message.names)
 
-        # Get name for every rank
-        bulk = 100 #Saves every 100 entries
-        for rank in range(1, on_message.names.maxRank + 1):
-            maxRetry = 3
-            for retry in range(maxRetry):
-                sleep(0.5)
-                on_message.names.requestCharacter(rank)
-
-                def check(message):
-                    return message.author.id == mudaeID and on_message.names.validCharacterPrefix(message.content, rank)
-
-                try:
-                    charRankMessage = await client.wait_for('message', check=check, timeout = 2.0)
-                except asyncio.TimeoutError:
-                    await message.channel.send("Mudae timed out. Retrying (" + str(retry + 1) + "/" + str(maxRetry) + ")")
-                    if(retry + 1 == maxRetry):
-                        logError("Mudae timed out for rank:" + str(rank))
-                        return
-                else:
-                    break
-
-            name = on_message.names.parseCharacterName(charRankMessage.content, rank)
-            if(name == False):
-                logError("Error requesting name from: " + charRankMessage.content)
-                continue
-
-            result = on_message.names.addName(name)
-            if(result == False):
-                logError("unable to add name: " + name + " at rank: " + str(rank))
-                continue
-
-            if(rank % bulk == 0):
-                print("Progress: ", (rank/on_message.names.maxRank) * 100, "%", sep='')
-                namesFile = open("names.txt", "w")
-                namesFile.write("Characters: " + str(rank) + "\n" + str(on_message.names.names))
-                namesFile.close()
-
-        namesFile = open("names", "w")
-        namesFile.write(str(on_message.names.names))
-        namesFile.close()
-
-
-
-        
 on_message.names = NameRetriever()
 
 client.run(TOKEN)
